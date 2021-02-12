330196594a4bef9d59850fc91da02848d451ef5f
Shreeyansh <shreeyanshshahi@gmail.com>
2021-02-12 11:51:04 +0530

    --added functionality to generate avatars url for guests

--- src/main/java/com/smartvoting/controller/GuestController.java
+++ src/main/java/com/smartvoting/controller/GuestController.java
@@ -10,17 +10,51 @@ import rx.Single;
 import rx.schedulers.Schedulers;
 
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/guest")
 public class GuestController {
+     private static String[] avatars={"https://imgur.com/v9dDexB",
+    "https://imgur.com/b36Pd3q",
+    "https://imgur.com/PBQRbcv",
+    "https://imgur.com/2BXhwbB",
+    "https://imgur.com/3uefIAv",
+    "https://imgur.com/6y37gQ1",
+    "https://imgur.com/Nhgocus",
+    "https://imgur.com/pNbYDLk",
+    "https://imgur.com/Ka2JeTn",
+    "https://imgur.com/FsqVqF4",
+    "https://imgur.com/ehqxeo8",
+    "https://imgur.com/yS7XTC6",
+    "https://imgur.com/KGl0vzD",
+    "https://imgur.com/c9nM3lI",
+    "https://imgur.com/7k0o6G0",
+    "https://imgur.com/38OcWn1",
+    "https://imgur.com/3u4slwM",
+    "https://imgur.com/iYeujzC",
+    "https://imgur.com/r31dhsc",
+    "https://imgur.com/7a1mzuB"
+    };
+public static int generateRandom(int min,int max) {
+    max=avatars.length;
+    int randomNum = ThreadLocalRandom.current().nextInt(min, max);
+    min++;
+    if(min==max)
+    {
+        min=max % min;
+    }
+    return randomNum;
+}
 
     @Autowired
     IGuestService iGuestService;
 
     @PostMapping(value="/addGuest")
     Single<Guest> addGuest(@RequestBody GuestDTO guest){
+        guest.setPhotoUrl(avatars[generateRandom(0,avatars.length)]);
         return iGuestService.addGuest(guest)
                 .subscribeOn(Schedulers.io());
 
